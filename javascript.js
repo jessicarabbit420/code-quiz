383e188b526ba62036bae76b533eaadb8fb77423
Sara Webb <saracharlotterose@gmail.com>
2020-06-17 18:56:50 -0700

    saving changes

--- javascript.js
+++ javascript.js
@@ -1,84 +1,130 @@
-
-// create button
-var startButton = document.createElement("button")
-var aButton = document.getElementById("aButton")
-var bButton =document.createElement("button")
-//set text content for button
- 
+var questionPrompt = document.getElementById("question");
+var pastScoresText = document.getElementById("pastScores");
+var localStorage = window.localStorage;
 
 
+var scoreText = document.getElementById("score");
+var trueBtn = document.getElementById("abutton");
+var falseBtn = document.getElementById("bbutton");
+trueBtn.disabled = true;
+falseBtn.disabled = true;
 
-startButton.textContent("click me to start quiz")
-// add event listener to start timer 
-startButton.addEventListener( onclick [ quizTimer]);
-// creating function quiztimer 
-function quizTimer(){
-  var quizTimer = setTimeout(() => {
-
-    if (aButton.addEventListener(onclick,"abutton"))
-    prompt("you clicked me !")
+var questions = [ "Is grass green?", "Is New York a country?", "Is fire hot?", "Is the sky blue?"]
+var correctAnswers = [true, false, true, true];
 
 
 
+var startBtn = document.getElementById("startButton");
+var score = 0;  
+scoreText.innerHTML = score;  
+var timer = document.getElementById("timer");
+
+var timeRemaining = 60;
+timer.innerHTML = timeRemaining;
 
 
 
+startBtn.onclick = function() {
+    var index = 0;
+    timeRemaining = 60;
+    timer.innerHTML = timeRemaining;
+    score = 0;  
+    scoreText.innerHTML = score;  
+    pastScoresText.innerHTML = "";
 
+    startBtn.disabled = true;
+    trueBtn.disabled = false;
+    falseBtn.disabled = false;
+    questionPrompt.innerHTML = questions[index];
 
-      
-   900000});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-	function showQuestions(questions, quizContainer){
-		// code will go here
-	}var myQuestions = [
-        {
-            question: "What is 10/2?",
-            answers: {
-                a: '3',
-                b: '5',
-                c: '115'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: "What is 30/3?",
-            answers: {
-                a: '3',
-                b: '5',
-                c: '10'
-            },
-            correctAnswer: 'c'
+    trueBtn.onclick = function() { //true button functionality
+        if(correctAnswers[index] == true) { //the answer is correct
+            score++;
+            scoreText.innerHTML = score; 
         }
-    ];
+        else { //answer is incorrect
+            timeRemaining = timeRemaining - 5;
+            timer.innerHTML = timeRemaining;        
+        }
+        index++;
+        //we want to check if index > length of the question array, if it is then we STOP
+        if(index >= questions.length) {
+            endGame();
+        }
+        else {
+            questionPrompt.innerHTML = questions[index];
+        }
+        
+    }
 
-	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
-	}
+    falseBtn.onclick = function() { //false button functionality
+        if(correctAnswers[index] == false) { //the answer is correct
+            score++;
+            scoreText.innerHTML = score; 
+        }
+        else { //answer is incorrect
+            timeRemaining = timeRemaining - 5;
+            timer.innerHTML = timeRemaining;
+        }
+        index++;
+        if(index >= questions.length) {
+            endGame();
+        }
+        else {
+            questionPrompt.innerHTML = questions[index];
+        }
+    }
 
-	// show the questions
-	showQuestions(questions, quizContainer);
 
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
+    gameTimer = setInterval(() => {
+        timeRemaining--;
+        timer.innerHTML = timeRemaining;
+        if(timeRemaining <= 0) {    
+            endGame();
+        }
+    }, 1000);
+
+};
+
+
+function endGame(){
+    startBtn.disabled = false;
+    trueBtn.disabled = true;
+    falseBtn.disabled = true;
+    questionPrompt.innerHTML = "";
+    clearInterval( gameTimer);
+    var initialSave = prompt("Enter Initials here");
+    if(initialSave != null) {
+        var previousScores = JSON.parse(localStorage.getItem(initialSave));
+        if(previousScores == null) { //first unique entry for initials
+            previousScores = [];
+        }
+        previousScores.push(score);
+        localStorage.setItem(initialSave, JSON.stringify(previousScores))
+        renderPastScores();
+    }
 }
+
+function renderPastScores() {
+    for(var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        var value = localStorage.getItem(key);
+
+        pastScoresText.innerHTML += `${key}: ${value}<br>`;
+    }
 }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
